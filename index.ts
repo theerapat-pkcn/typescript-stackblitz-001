e7be8f325382031db9669b16a51c80c331b1765e
theerapat-pkcn <theerapat.pkcn@gmail.com>
2021-06-23 00:58:10 +0700

    1 file modified

--- index.ts
+++ index.ts
@@ -17,15 +17,33 @@
 
 // observer.next("Something");
 
-import { oPerson, Person } from "./sub";
+import { oPerson, Person } from './sub';
 
 // Object type
-oPerson.fullName = "Theerapat Singprasert";
+oPerson.fullName = 'Theerapat Singprasert';
 console.log(oPerson);
 console.log(oPerson.fullName);
 
 // Class type
-let bai: Person = new Person("Theerapat", "Singprasert");
+let bai: Person = new Person('Theerapat', 'Singprasert');
 console.log(bai);
 console.log(bai.fullName);
 console.log(`${bai.getFullName()} (by method type)`); //Not Recommended
+
+function asyncAction() {
+  var promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Async is done!");
+      reject('Rejected!');
+    }, 1500);
+  });
+  return promise;
+}
+
+asyncAction().then(function(success) { 
+    console.log(success); 
+}) 
+.catch(function(error) { 
+   // error handler is called
+   console.log(error); 
+});
